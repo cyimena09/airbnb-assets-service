2e05262226dbe18d6fd5bb31f091a264080b4bbe
Emile Cyimena <cyimena09@hotmail.com>
2021-03-11 21:07:54 +0100

    Initial commit

---
+++ src/test/java/be/cyimena/airbnbassetsservice/AirbnbAssetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cyimena.airbnbassetsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbAssetsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
